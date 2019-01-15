5b83bf79e3fbabcc4e866d41055ce4de599384ef
KNobles <nobles-k@hotmail.com>
2019-01-15 09:31:02 +0100

    création userControl

---
+++ src/main/java/be/helha/aemt/control/UserControl.java
@@ -0,0 +1,48 @@
+package be.helha.aemt.control;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+import be.helha.aemt.ejb.ManagementUserEJB;
+import be.helha.aemt.entities.User;
+
+@RequestScoped
+@Named
+public class UserControl {
+
+	@EJB
+	private ManagementUserEJB bean;
+	private User user = new User();
+	
+	public List<User> doSelectAll(){
+		return bean.selectAll();
+	}
+	
+	public ManagementUserEJB getBean() {
+		return bean;
+	}
+
+	public void setBean(ManagementUserEJB bean) {
+		this.bean = bean;
+	}
+	public String doAdd() {
+		bean.add(user);
+		return "liste.xhtml";
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+//	public String doGetDetails(User user) {
+//		this.user = user;
+//		return "detail.xhtml";
+//	}
+}
