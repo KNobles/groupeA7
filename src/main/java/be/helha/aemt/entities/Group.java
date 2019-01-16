cf01d918ff25c5f113e4b3c09ccac2c2ac4e70d6
Laetitia <laetitia.nilles@gmail.com>
2019-01-16 09:49:28 +0100

    Class group updated

--- src/main/java/be/helha/aemt/entities/Group.java
+++ src/main/java/be/helha/aemt/entities/Group.java
@@ -25,4 +25,34 @@ public class Group implements Serializable {
 	public Group() {
 		
 	}
+	
+	public Group(String name) {
+		this.name = name;
+	}
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<User> getUsers() {
+		return this.users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+	
+	
 }
