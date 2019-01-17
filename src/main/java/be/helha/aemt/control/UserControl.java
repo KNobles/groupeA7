0994b22572681ed5964b31f090f07ad67c7badb8
Laetitia <laetitia.nilles@gmail.com>
2019-01-17 11:32:27 +0100

    Débug connexion sécurisée

--- src/main/java/be/helha/aemt/control/UserControl.java
+++ src/main/java/be/helha/aemt/control/UserControl.java
@@ -8,7 +8,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.ejb.EJB;
-import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
@@ -20,13 +20,14 @@ import javax.servlet.http.HttpSession;
 import be.helha.aemt.ejb.ManagementUserEJB;
 import be.helha.aemt.entities.Users;
 
-@RequestScoped
-@Named
+@SessionScoped //le temps de la session config dans web.xml, ou d'une requête
+//on ne va pas toujours l'utiliser, il faut le mettre en passif en attendant son utilisation -> implements Serializable
+@Named //annotation d'injection !ne pas utiliser ManagedBean! //sert au serveur pour injecter ce composant dans xhmtl
 public class UserControl implements Serializable {
 
 	@EJB
 	private ManagementUserEJB bean;
-	private Users user = new Users();
+	private Users authenticatedUser = new Users();
 	private boolean admin;
 	
 	private static Logger log = Logger.getLogger(UserControl.class.getName());
@@ -38,20 +39,20 @@ public class UserControl implements Serializable {
 	public void setAdmin(boolean admin) {
 		this.admin = admin;
 	}
-
-	public Users getUser() {
-		return this.user;
-	}
-
-	public void setUser(Users user) {
-		this.user = user;
-	}
 	
+	public Users getAuthenticatedUser() {
+		return this.authenticatedUser;
+	}
+
+	public void setAuthenticatedUser(Users authenticatedUser) {
+		this.authenticatedUser = authenticatedUser;
+	}
+
 	public String doValidatePassword() {
 		//vérification des champs requis
-		if(this.user.getPassword().equals("") //mot de passe
-			|| this.user.getFirstname().equals("") //prénom
-			|| this.user.getName().equals("")) { //nom
+		if(this.authenticatedUser.getPassword().equals("") //mot de passe
+			|| this.authenticatedUser.getFirstname().equals("") //prénom
+			|| this.authenticatedUser.getName().equals("")) { //nom
 			
 			return "createUser.xhtml"; 
 		}
@@ -67,8 +68,8 @@ public class UserControl implements Serializable {
 	}
 	
 	public String doAdd() {
-		log.info(this.user.toString() + " a bien été ajouté comme relais d'aide à la réussite!");
-		this.bean.add(this.user);
+		log.info(this.authenticatedUser.toString() + " a bien été ajouté comme relais d'aide à la réussite!");
+		this.bean.add(this.authenticatedUser);
 		return "displayUsers.xhtml";
 	}
 
@@ -76,17 +77,17 @@ public class UserControl implements Serializable {
 		FacesContext context = FacesContext.getCurrentInstance();
 		HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
 		try {
-			request.login(this.user.getMail(), this.user.getPassword());
+			request.login(this.authenticatedUser.getMail(), this.authenticatedUser.getPassword());
 		} catch (ServletException e) {
 			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Connexion échouée!", null));
 			return "loginerror.xhtml";
 		}
 		Principal principal = request.getUserPrincipal();
-		this.user = this.bean.searchByMail(principal.getName());
+		this.authenticatedUser = this.bean.searchByMail(principal.getName());
 		log.info("Authentication réussie pour l'utilisateur : " + principal.getName());
 		ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
 		Map<String, Object> sessionMap = externalContext.getSessionMap();
-		sessionMap.put("Users", this.user);
+		sessionMap.put("Users", this.authenticatedUser);
 		if(request.isUserInRole("relay"))
 			return "relay/home.xhtml?faces-redirect=true";
 		//else if(request.isUserInRole("admin"))
@@ -97,7 +98,7 @@ public class UserControl implements Serializable {
 		FacesContext context = FacesContext.getCurrentInstance();
 		HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
 		try {
-			this.user = null;
+			this.authenticatedUser = null;
 			request.logout();
 			// clear the session
 			((HttpSession) context.getExternalContext().getSession(false)).invalidate();
