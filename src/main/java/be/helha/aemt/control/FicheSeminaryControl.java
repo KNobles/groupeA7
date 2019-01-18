f3e7128369f1b4eb6ad13d07fb53540ccd586a1f
Laetitia <laetitia.nilles@gmail.com>
2019-01-18 03:48:17 +0100

    corrections

--- src/main/java/be/helha/aemt/control/FicheSeminaryControl.java
+++ src/main/java/be/helha/aemt/control/FicheSeminaryControl.java
@@ -37,7 +37,7 @@ public class FicheSeminaryControl implements Serializable{
 	
 	public String buildSeminary(Seminary seminary) {		
 		this.seminary = seminary;
-		return "${pageContext.request.contextPath}/relay/ficheSeminary.xhtml";
+		return "ficheSeminary.xhtml";
 	}
 	
 	public Student doAddStudent() {
